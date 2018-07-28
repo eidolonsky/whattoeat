669a5602a060797244d999f7432113eb119918b6
TianJiang <riersy2324@gmail.com>
2018-07-28 17:15:26 +0800

    create: replicate the whattoeat to a new directory

---
+++ script.js
@@ -0,0 +1,40 @@
+/*whattoeat*/
+var foods = [
+"BBQ", "Hot Pot", "Sushi", "泉鲤", "新加坡鱼酿", 
+"Dumpling", "乐凯撒", "Burger King", "Fried Rice", 
+"Ramen", "Ice Cream", "螺蛳粉", "汤饭", "凉皮", 
+ "Crab Cake", "煲仔饭", "Rum Milkshake",
+ "Bamboo", "Kiwi", "烧腊", "Fried Shrimp", 
+ "Peking Duck", "KFC", "McDonald", "冒菜", "焖饭",
+ "Poke Bowl", "Steak House", "Salad", "Rice Noodle",
+ "Pork Feet", "拌饭", "麻辣香锅", "麻辣烫", "本帮菜", 
+ "串串香", "汤包", "豆花", "脑花", "卤菜"
+]; 
+
+var food;
+var index = -1;
+var time;  
+
+function startdraw(){  
+  document.getElementById("buttonStart").disabled = true;
+  document.getElementById("onearmbandit").src = "/oneArmBanditDown.png";  
+  drawfood();  
+}  
+function drawfood(){
+  if(foods.length > 0){
+    index = Math.floor(Math.random()*100 % foods.length);  
+    food = foods[index];  
+    document.getElementById("result").innerHTML = food;  
+
+    time = window.setTimeout(drawfood, 8.12);  
+  }
+  else{
+      document.getElementById("result").innerHTML = "Nah";  
+  }
+}  
+function enddraw(){  
+  window.clearTimeout(time);  
+  document.getElementById("buttonStart").disabled = false;
+  document.getElementById("onearmbandit").src = "/oneArmBandit.png";    
+  foods.splice(index, 1);
+} 
